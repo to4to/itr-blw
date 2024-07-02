d7c2f2737d7645f828caea0e9dd86924bd55f1f8
to4to <justjustifying@outlook.com>
2024-07-02 23:12:49 +0530

    chore: Update go.mod with new dependencies

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/to4to/itr-blw
 go 1.22.3
 
 require (
-	github.com/go-chi/chi v1.5.5 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/go-chi/chi/v5 v5.1.0
+	github.com/joho/godotenv v1.5.1
 )
--- main.go
+++ main.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	godotenv.Load()
 
+	godotenv.Load()
 	port := os.Getenv("PORT")
 
 	if port == "" {
-		log.Fatal("PORT Connection Failed")
+		log.Fatal("Port Not Bound ..failed to connect from env")
+
 	}
 
+	fmt.Println("PORT :", port)
+
+	router := chi.NewRouter()
+
+	server := &http.Server{
+
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
